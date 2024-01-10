5fd478450454c04113d39ce9beb590ecda85f978
Tauhid Musa Rieon <156216941+tauhidmusarieon@users.noreply.github.com>
2024-01-10 21:49:39 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;
+    return 0;
+}
