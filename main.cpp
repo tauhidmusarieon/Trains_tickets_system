69c82f43c5669ad9c4c196ab034528d621a47cbb
Tauhid Musa Rieon <156216941+tauhidmusarieon@users.noreply.github.com>
2024-01-11 00:50:08 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,1 @@
-#include <iostream>
 
-using namespace std;
-
-int main()
-{
-    cout << "Hello world!" << endl;
-    cout << "Hello world!" << endl;
-    cout << "Hello world!" << endl;
-    cout << "Hello world!" << endl;
-    return 0;
-}
